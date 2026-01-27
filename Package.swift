fe4aa7e1a4a2efc8a219f5ef5a0eb59696ad2705
Marius <houseinblues@gmail.com>
2026-01-27 15:04:43 +0200

    Make CZlib available for Static Linux SDK

--- Package.swift
+++ Package.swift
@@ -2,64 +2,40 @@
 
 import PackageDescription
 
-var targets: [PackageDescription.Target] = []
-
-// Declare CZlib only on Linux.
-#if os(Linux)
-targets.append(
-    .systemLibrary(
-        name: "CZlib",
-        path: "Sources/CZlib"
-    )
-)
-#endif
-
-var swiftGzipDependencies: [Target.Dependency] = []
-#if os(Linux)
-swiftGzipDependencies.append(.target(name: "CZlib"))
-#endif
-
-var swiftGzipLinkerSettings: [LinkerSetting] = []
-#if !os(Linux)
-// On Linux, CZlib's modulemap already links `-lz`.
-swiftGzipLinkerSettings.append(.linkedLibrary("z"))
-#endif
-
-targets.append(
-    .target(
-        name: "SwiftGzip",
-        dependencies: swiftGzipDependencies,
-        path: "Sources",
-        resources: [.copy("Resources/PrivacyInfo.xcprivacy")],
-        swiftSettings: [.enableExperimentalFeature("StrictConcurrency")],
-        linkerSettings: swiftGzipLinkerSettings
-    )
-)
-
-targets.append(
-    .testTarget(
-        name: "SwiftGzipTests",
-        dependencies: [.target(name: "SwiftGzip")],
-        resources: [
-            .copy("Resources/test.png"),
-            .copy("Resources/test.png.gz")
-        ]
-    )
-)
-
 let package = Package(
     name: "swift-gzip",
     platforms: [
         .iOS(.v14),
-        .tvOS(.v14),
-        .visionOS(.v1),
         .macOS(.v12),
         .macCatalyst(.v14),
+        .tvOS(.v14),
+        .visionOS(.v1),
         .watchOS(.v8)
     ],
     products: [
         .library(name: "SwiftGzip", targets: ["SwiftGzip"])
     ],
-    targets: targets,
+    targets: [
+        .target(
+            name: "SwiftGzip",
+            dependencies: [.target(name: "CZlib", condition: .when(platforms: [.linux]))],
+            path: "Sources",
+            resources: [.copy("Resources/PrivacyInfo.xcprivacy")],
+            swiftSettings: [.enableExperimentalFeature("StrictConcurrency")],
+            linkerSettings: [.linkedLibrary("z", .when(platforms: [.iOS, .macOS, .macCatalyst, .tvOS, .visionOS, .watchOS]))]
+        ),
+        .testTarget(
+            name: "SwiftGzipTests",
+            dependencies: [.target(name: "SwiftGzip")],
+            resources: [
+                .copy("Resources/test.png"),
+                .copy("Resources/test.png.gz")
+            ]
+        ),
+        .systemLibrary(
+            name: "CZlib",
+            path: "Sources/CZlib"
+        )
+    ],
     swiftLanguageModes: [.v6]
 )
